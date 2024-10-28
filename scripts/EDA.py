effe39ec166e31a7ede7cdc2c4e88324da04c1c7
Ubuntu <ubuntu@ip-172-31-16-127.ec2.internal>
2024-10-28 02:44:49 +0000

    Continuacion EDA

--- scripts/EDA.py
+++ scripts/EDA.py
@@ -13,4 +13,22 @@ df = pd.read_csv(url, encoding='ISO-8859-1')
 
 df=df.rename(columns={'Ship ID - Ship Classification':'Vessel Type','Ship - Name':'Ship Name'})
 
-print(df)
+#Evaluacion de los datos
+print(df.info())
+
+print(df.isnull().sum())
+
+print(msno.matrix(df))
+
+#Limpieza de los datos
+df_clean=df[['Ship Name','Vessel Type','Country','Location','Arrival','Departure','Region Name','Status']]
+df_clean = df_clean.dropna(subset=['Arrival','Departure','Ship Name','Location','Vessel Type','Status'])
+df_clean = df_clean[df_clean['Status'] != 'Cancelled']
+# Convertir las columnas 'Arrival' y 'Departure' a datetime
+df_clean['Arrival'] = pd.to_datetime(df_clean['Arrival'], errors='coerce')
+df_clean['Departure'] = pd.to_datetime(df_clean['Departure'], errors='coerce')
+df_clean['Duration'] = (df_clean['Departure'] - df_clean['Arrival']).dt.days
+# Filtrar las filas donde 'Duration' sea menor o igual a 60 días
+df_clean = df_clean[(df_clean['Duration'] <= 60) & (df_clean['Duration'] >= 0)]
+
+print(df_clean)
