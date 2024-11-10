0d9b2b9aebd1114a7ae7669db1a8f1ee62347cee
Andres Arcila <arcilacarmona@gmail.com>
2024-11-10 02:41:47 -0500

    añadido modelo predictivo

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from scripts.dashboard import initialize_app, generate_control_card, plot_heatmap
 from scripts.ETL import load_data
+from scripts.modelo import rand_class
 from dash import html
 from dash.dependencies import Input, Output
+
 
 # Inicializar la aplicación
 app = initialize_app()
@@ -11,7 +13,7 @@ server = app.server
 datasource='data/RTOP.csv'
 data=load_data(datasource)
 # Uso de la función con el dataset df_clean
-
+location_accuracy,location_mse,location_r2,predicted_location=rand_class(data)
 
 # Layout de la aplicación
 app.layout = html.Div(
@@ -25,7 +27,7 @@ app.layout = html.Div(
                 html.Div(generate_control_card(data), style={ 'width': '30%','margin':'auto'}),
                 html.Br(),
                 html.P("El modelo predice que el proximo puerto es:"),
-                html.H4('PONCE',style={'color':'Red'}),
+                html.H4(id='predicted-location',style={'color':'Red'}),
                 ], 
             style={'width': '100%', 'textAlign': 'center', 'padding': '20px'} 
         ),
@@ -34,9 +36,9 @@ app.layout = html.Div(
             className="four columns",
             children=[
                 html.H4("Estadisticas del modelo"),
-                html.P("Precision de Ubicación: 75.00%"),
-                html.P("Error Cuadrático Medio de Ubicación: 0.25"),
-                html.P('R^2 de Ubicación: 0.95'),
+                html.P(id="location-accuracy"),
+                html.P(id="location-mse"),
+                html.P(id="location-r2"),
             ]
         ),
         html.Div(
@@ -56,16 +58,28 @@ app.layout = html.Div(
 )
 
 # Callback para actualizar la gráfica
-@app.callback(
-    Output("folium-map", "srcDoc"),
-    [Input("dropdown-Ship-Name", "value")
+@app.callback([
+        Output("folium-map", "srcDoc"),
+        Output("predicted-location", "children"),
+        Output("location-accuracy", "children"),
+        Output("location-mse", "children"),
+        Output("location-r2", "children")
+    ],
+    [
+        Input("dropdown-Ship-Name", "value")
     ]
 )
 
 
 def update_plot(ShipName):
+    if not ShipName: 
+        return "", "", "", "", ""
     map_html = plot_heatmap(data,ShipName)
-    return map_html
+    predicted_location,location_accuracy,location_mse,location_r2  = rand_class(data, ShipName) 
+    accuracy_text = f"Precisión de Ubicación (Clasificación): {location_accuracy * 100:.2f}%" 
+    mse_text = f"Error Cuadrático Medio de Ubicación: {location_mse:.2f}" 
+    r2_text = f"R^2 de Ubicación: {location_r2:.2f}"
+    return map_html, predicted_location,accuracy_text, mse_text, r2_text
 
 if __name__ == "__main__":
     app.run_server(host="0.0.0.0", debug=True)
